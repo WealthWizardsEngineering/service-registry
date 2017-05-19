92873ffb4b2f8c541cf48fd301fc52af54889096
Dave Shepherd <david.shepherd@wealthwizards.com>
2017-05-19 10:58:00 +0100

    Mod: upgrade ww-logging and remove console.log statements

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 require('dotenv').config({ silent: true });
 const env = require('./src/env-vars');
 const { app } = require('./src/server');
+const logger = require('./src/logger');
 
 const server = app.listen(env.PORT, () => {
-  console.log(`Listening on port ${env.PORT} with context route of /service-registry`);
+  logger.info(`Listening on port ${env.PORT} with context route of /service-registry`);
 });
 
 process.on('SIGTERM', () => {
-  console.log('Shutting down...');
+  logger.info('Shutting down...');
   server.close(() => app.shutdown());
 });
